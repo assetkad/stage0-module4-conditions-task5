6ad22e7c400316a13da0cb186190e15962fa5417
Asset Kad <asset_kad@mail.ru>
2023-05-08 15:25:51 +0500

    finished stage 0, module #4.1, task#5

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitchBreaks.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitchBreaks.java
@@ -1,17 +1,21 @@
 package school.mjc.stage0.conditions.task5;
 
 public class BasicSwitchBreaks {
-    public static void main(String[] args) {
-        int a = 20;
-        switch (a) {
-            case 7:
-                System.out.println("seven");
-            case 10:
-                System.out.println("ten");
-            case 20:
-                System.out.println("twenty");
-            default:
-                System.out.println("default");
-        }
+  public static void main(String[] args) {
+    int a = 20;
+    switch (a) {
+      case 7:
+        System.out.println("seven");
+        break;
+      case 10:
+        System.out.println("ten");
+        break;
+      case 20:
+        System.out.println("twenty");
+        break;
+      default:
+        System.out.println("default");
+        break;
     }
+  }
 }
